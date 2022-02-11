dd6e5d1417fa4aee786ba05fde2194b187485a05
Lartg <roc.gerrety@gmail.com>
2022-02-10 19:38:47 -0800

    added login/signup

--- grocery_app/extensions.py
+++ grocery_app/extensions.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from grocery_app.config import Config
+from flask_login import LoginManager
 import os
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+from .models import User
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+bcrypt = Bcrypt(app)
 
 db = SQLAlchemy(app)
